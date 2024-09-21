06a947511736354d7f9d4d80c6956482f642afc2
vipwhy12 <vipwhy12@naver.com>
2024-09-22 02:02:03 +0900

    refactor: point 함수 컨트롤러 로직 서비스 레이러로 분리

--- src/point/point.service.ts
+++ src/point/point.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { UserPoint } from './point.model';
 
 @Injectable()
-export class PointService {}
+export class PointService {
+  constructor() {}
+
+  //TODO - 특정 유저의 포인트를 조회하는 기능을 작성해주세요.
+  async point(id: number): Promise<UserPoint> {
+    return { id: id, point: 0, updateMillis: Date.now() };
+  }
+}
