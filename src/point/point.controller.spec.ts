6e25731df91e1fce7cf0e2e0db3af49355104d86
vipwhy12 <vipwhy12@naver.com>
2024-09-22 05:31:27 +0900

    test: point 함수의 ID 검증을 위한 단위 테스트 구현 (숫자가 아니거나 유효하지 않은 경우 처리)

--- src/point/point.controller.spec.ts
+++ src/point/point.controller.spec.ts
@@ -2,6 +2,7 @@ import { PointService } from './point.service';
 import { PointController } from './point.controller';
 import { Test, TestingModule } from '@nestjs/testing';
 import { createMock, DeepMocked } from '@golevelup/ts-jest';
+import { BadRequestException } from '@nestjs/common';
 
 describe('PointController', () => {
   let pointController: PointController;
@@ -25,6 +26,19 @@ describe('PointController', () => {
   describe('PointController', () => {
     it('should be defined', () => {
       expect(true).toBe(true);
+    });
+  });
+
+  describe('point', () => {
+    /**
+     * 클라이언트가 숫자로 변환할 수 없는 문자열을 ID로 전달했을 때,
+     * 컨트롤러가 올바르게 BadRequestException을 발생시키는지 확인합니다.
+     */
+    it('id가 숫자가 아니면 실패한다.', async () => {
+      const invalidId = '숫자형으로 바꿀 수 없습니다.';
+      const result = pointController.point(invalidId);
+
+      await expect(result).rejects.toBeInstanceOf(BadRequestException);
     });
   });
 });
