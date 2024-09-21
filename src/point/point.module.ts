2a868829611548dec3e4fa5cd9543ab79c9e82eb
vipwhy12 <vipwhy12@naver.com>
2024-09-22 01:45:56 +0900

    chore: point service 계층 생성

--- src/point/point.module.ts
+++ src/point/point.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { PointController } from './point.controller';
 import { DatabaseModule } from 'src/database/database.module';
+import { PointService } from './point.service';
 
 @Module({
   imports: [DatabaseModule],
   controllers: [PointController],
+  providers: [PointService],
 })
 export class PointModule {}
