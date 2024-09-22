a2c179f6d823e9fff443d53109671866b47336fc
vipwhy12 <vipwhy12@naver.com>
2024-09-23 01:07:38 +0900

    refactor: pointController의 point 조회 단위 테스트 개선

--- src/point/point.controller.spec.ts
+++ src/point/point.controller.spec.ts
@@ -2,10 +2,6 @@ import { PointService } from './point.service';
 import { PointController } from './point.controller';
 import { Test, TestingModule } from '@nestjs/testing';
 import { createMock, DeepMocked } from '@golevelup/ts-jest';
-import {
-  BadRequestException,
-  InternalServerErrorException,
-} from '@nestjs/common';
 
 describe('PointController', () => {
   let pointController: PointController;
@@ -26,83 +22,34 @@ describe('PointController', () => {
     pointService = module.get(PointService);
   });
 
-  describe('PointController', () => {
-    it('should be defined', () => {
-      expect(true).toBe(true);
-    });
-  });
+  describe('point 조회시,', () => {
+    describe('id가 1보다 작으면', () => {
+      const invalidId = -1;
+      const errorMessage = 'ID는 1보다 커야 합니다';
 
-  describe('point', () => {
-    describe('point 함수가 실패한다.', () => {
-      // ID가 숫자로 변환되지 않을 경우 BadRequestException을 발생시켜야 한다.
-      it('id가 숫자가 아니면 실패한다.', async () => {
-        const invalidId = '숫자형으로 바꿀 수 없습니다.';
-        const result = pointController.point(invalidId);
+      // 유효하지 않은 ID로 조회할 때, 컨트롤러는 서비스가 던지는 에러를 그대로 반환하는지 테스트합니다.
+      it('실패 응답을 반환한다.', async () => {
+        pointService.point.mockRejectedValue(new Error(errorMessage));
 
-        await expect(result).rejects.toBeInstanceOf(BadRequestException);
-      });
-
-      // ID가 0 이하일 경우 BadRequestException을 발생시켜야 한다.
-      it('id가 0보다 작으면 실패한다.', async () => {
-        const invalidId = 0;
-        const result = pointController.point(invalidId);
-
-        await expect(result).rejects.toBeInstanceOf(BadRequestException);
-      });
-
-      // ID가 undefined일 경우 BadRequestException을 발생시켜야 한다.
-      it('id가 undefined이면 실패한다.', async () => {
-        const invalidId = undefined;
-        const result = pointController.point(invalidId);
-
-        await expect(result).rejects.toBeInstanceOf(BadRequestException);
-      });
-
-      // ID가 null일 경우 BadRequestException을 발생시켜야 한다.
-      it('id가 null이면 실패한다.', async () => {
-        const invalidId = null;
-        const result = pointController.point(invalidId);
-
-        await expect(result).rejects.toBeInstanceOf(BadRequestException);
-      });
-
-      // PointService.point 에서 반환하는 값이 비어있으면 InternalServerErrorException을 발생시켜야 한다.
-      it('서비스 레이어의 포인트 함수에서 반환하는 값이 비어있으면 실패한다.', async () => {
-        const validId = '1';
-
-        pointService.point.mockResolvedValue(null);
-
-        const result = pointController.point(validId);
-
-        await expect(result).rejects.toBeInstanceOf(
-          InternalServerErrorException,
+        await expect(pointController.point(invalidId)).rejects.toThrowError(
+          errorMessage,
         );
+        expect(pointService.point).toHaveBeenCalledWith(invalidId);
       });
     });
 
-    describe('point 함수가 성공한다.', () => {
-      // 서비스 레이어의 Point 함수가 정상적인 값을 반환 시, 컨트롤러의 Point 함수가 해당 값을 올바르게 반환하는지 확인합니다.
-      it('서비스 레이어에서 정상적인 값을 반환하면 성공한다.', async () => {
-        const validId = 1;
-        const expectedResult = { id: 1, point: 1, updateMillis: Date.now() };
+    describe('id가 1보다 크면', () => {
+      const validId = 1;
+      const expectedResult = { id: 1, point: 1, updateMillis: Date.now() };
 
+      // 유효한 ID로 조회할 때, 컨트롤러는 서비스가 던지는 응답을 그대로 반환하는지 테스트 합니다.
+      it('성공 응답을 반환한다.', async () => {
         pointService.point.mockResolvedValue(expectedResult);
 
         const result = await pointController.point(validId);
 
         expect(result).toEqual(expectedResult);
-      });
-
-      //컨트롤러가 서비스 레이어의 point함수에 올바른 파라미터를 전달하여 호출하는지 확인합니다.
-      it('서비스 레이어의 point 함수가 올바르게 호출되는지 확인한다.', async () => {
-        const validId = 1;
-        const expectedResult = { id: 1, point: 1, updateMillis: Date.now() };
-
-        pointService.point.mockResolvedValue(expectedResult);
-
-        await pointController.point(validId);
-
-        expect(pointService.point).toHaveBeenCalledWith(1);
+        expect(pointService.point).toHaveBeenCalledWith(validId);
       });
     });
   });
