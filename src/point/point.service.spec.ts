137be0306108cad487461c6dd56aaa212c0042d2
vipwhy12 <vipwhy12@naver.com>
2024-09-23 17:57:55 +0900

    feat: 사용자 포인트 조회 구현

--- src/point/point.service.spec.ts
+++ src/point/point.service.spec.ts
@@ -1,18 +1,57 @@
+import { createMock, DeepMocked } from '@golevelup/ts-jest';
 import { Test, TestingModule } from '@nestjs/testing';
 import { PointService } from './point.service';
+import { UserPointTable } from '../database/userpoint.table';
+import { UserPoint } from './point.model';
 
 describe('PointService', () => {
-  let service: PointService;
+  let pointService: PointService;
+  let userPointTable: DeepMocked<UserPointTable>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [PointService],
+      controllers: [],
+      providers: [
+        PointService,
+        {
+          provide: UserPointTable,
+          useValue: createMock<UserPointTable>(),
+        },
+      ],
     }).compile();
 
-    service = module.get<PointService>(PointService);
+    pointService = module.get<PointService>(PointService);
+    userPointTable = module.get(UserPointTable);
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  describe('getPointById', () => {
+    describe('id가 유효하지않으면', () => {
+      const validId = -3;
+      const errorMessage = 'Database error';
+
+      it('실패한다.', async () => {
+        userPointTable.selectById.mockRejectedValue(new Error(errorMessage));
+        const expectedResult = () => userPointTable.selectById(validId);
+        await expect(expectedResult).rejects.toThrow(errorMessage);
+      });
+    });
+
+    describe('id가 유효하면', () => {
+      const validId: number = 1;
+      const expectedResult: UserPoint = {
+        id: validId,
+        point: 100,
+        updateMillis: Date.now(),
+      };
+
+      it('성공적으로 유저 포인트를 반환한다.', async () => {
+        userPointTable.selectById.mockResolvedValue(expectedResult);
+
+        const result = await pointService.getPointById(validId);
+
+        expect(result).toEqual(expectedResult);
+        expect(userPointTable.selectById).toHaveBeenCalledWith(validId);
+      });
+    });
   });
 });
