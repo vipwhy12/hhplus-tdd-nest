ec6d2ff4228ee71bfa144a37293eeb8925a6dcaf
vipwhy12 <vipwhy12@naver.com>
2024-09-26 02:25:27 +0900

    refactor: 사용자 포인트 사용 코드 개선

--- src/point/point.service.ts
+++ src/point/point.service.ts
@@ -1,4 +1,4 @@
-import { TransactionType } from 'src/point/point.model';
+import { TransactionType } from '../point/point.model';
 import { PointRepository } from './point.repository';
 import { Injectable } from '@nestjs/common';
 import { PointHistory, UserPoint } from './point.model';
@@ -12,24 +12,19 @@ export class PointService {
     return await this.pointRepository.getPointById(id);
   }
 
-  //특정 유저의 포인트 충전/이용 내역을 조회하는 기능을 작성해주세요.
+  //특정 유저의 포인트 충전/이용 내역을 조회하는 기능
   async history(id: number): Promise<PointHistory[]> {
     return await this.pointRepository.getHistoryId(id);
   }
 
-  //특정 유저의 포인트를 충전하는 기능을 작성해주세요.
+  //특정 유저의 포인트를 충전하는 기능
   async charge(id: number, amount: number): Promise<UserPoint> {
     //TODO - Custom Error
     //TODO - 동시성 제어: 동시성 제어란 공유자원을 두고 타투는 싸움!
-    /** 행동분석
-     * 1. 유저의 point를 충전한다.
-     * 2. 유저의 history를 생성한다.
-     * 3. 결과를 반환한다.
-     */
 
-    const transactionType: TransactionType = TransactionType.CHARGE;
-    const userPoint: UserPoint = await this.upsertPoint(id, amount);
-    const userHistory = await this.setHisotryId(
+    const transactionType = TransactionType.CHARGE;
+    const userPoint = await this.pointRepository.upsertPoint(id, amount);
+    await this.pointRepository.setHisotryId(
       id,
       amount,
       transactionType,
@@ -39,52 +34,24 @@ export class PointService {
     return userPoint;
   }
 
+  //특정 유저의 포인트를 사용하는 기능
   async use(id: number, amount: number): Promise<UserPoint> {
-    //특정 유저의 포인트를 사용하는 기능을 작성해주세요.
-    //TODO - Custom Error
-    //TODO - 동시성 제어: 동시성 제어란 공유자원을 두고 타투는 싸움!
-    /** 행동분석
-     * 0. 유저의 포인트를 조회한다.
-     * 1. 유저의 포인트와 사용할 포인트를 대조한다.
-     * 2. 유저의 포인트가 충분하다면 사용한다.
-     *    -> 유저의 포인트 사용
-     *    -> 유저의 히스토리 생성
-     * 3. 유저의 포인트가 충분하지 않다면
-     *    -> 거절
-     */
     const transactionType: TransactionType = TransactionType.USE;
     const userPoint = await this.pointRepository.getPointById(id);
     const point = userPoint.point;
 
-    if (point < amount) {
-      throw Error('포인트가 충분하지 않습니다.');
-    }
+    if (point < amount) throw new Error(`${amount - point}point 부족`);
 
-    const useUserPoint = await this.upsertPoint(id, amount);
-    const userHistory = await this.setHisotryId(
+    //TODO - Custom Error & 포인트가 충분하지 않은
+    //TODO - 동시성 제어: 동시성 제어란 공유자원을 두고 타투는 싸움!
+
+    const useUserPoint = await this.pointRepository.upsertPoint(id, amount);
+    await this.pointRepository.setHisotryId(
       id,
       amount,
       transactionType,
       useUserPoint.updateMillis,
     );
     return useUserPoint;
-  }
-
-  async upsertPoint(id: number, amount: number) {
-    return await this.pointRepository.upsertPoint(id, amount);
-  }
-
-  async setHisotryId(
-    id: number,
-    amount: number,
-    transactionType: TransactionType,
-    updateMillis: number,
-  ) {
-    return await this.pointRepository.setHisotryId(
-      id,
-      amount,
-      transactionType,
-      updateMillis,
-    );
   }
 }
