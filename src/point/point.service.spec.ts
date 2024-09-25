df145ee7556d6145665f5ea6caf3ace19113e3f0
vipwhy12 <vipwhy12@naver.com>
2024-09-26 02:24:41 +0900

    test: 사용자 포인트 사용 단위 테스트 코드 구현

--- src/point/point.service.spec.ts
+++ src/point/point.service.spec.ts
@@ -1,11 +1,11 @@
 import { createMock, DeepMocked } from '@golevelup/ts-jest';
 import { Test, TestingModule } from '@nestjs/testing';
 import { PointService } from './point.service';
-import { PointRepository } from '../point/point.repository';
+import { PointRepository } from './point.repository';
 
 describe('PointService', () => {
   let pointService: PointService;
-  let userRepository: DeepMocked<PointRepository>;
+  let pointRepoitory: DeepMocked<PointRepository>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -20,10 +20,63 @@ describe('PointService', () => {
     }).compile();
 
     pointService = module.get<PointService>(PointService);
-    userRepository = module.get(PointRepository);
+    pointRepoitory = module.get(PointRepository);
   });
 
   it('should be defined', () => {
     expect(pointService).toBeDefined();
+  });
+
+  describe('getPointById', () => {});
+
+  describe('history', () => {});
+
+  describe('charge', () => {
+    /** 행동분석
+     * 1. 유저의 point를 충전한다.
+     * 2. 유저의 history를 생성한다.
+     * 3. 결과를 반환한다.
+     */
+  });
+
+  describe('use', () => {
+    /** 행동분석
+     * 1. 유저의 포인트를 조회한다.
+     * 2. 유저의 포인트와 사용할 포인트를 대조한다.
+     * 3. 유저의 포인트가 충분하다면 사용한다.
+     * 4. 유저의 포인트가 충분하지 않다면
+     */
+
+    describe('포인트가 충분하지 않을 때', () => {
+      //Given
+      const id = 1;
+      const amout = 50000;
+      const userPoint = { id, point: 1000, updateMillis: 20 };
+      const expectResult = Error;
+
+      //TODO ERR
+      it('거절한다.', async () => {
+        pointRepoitory.getPointById.mockResolvedValue(userPoint);
+
+        await expect(pointService.use(id, amout)).rejects.toBeInstanceOf(
+          expectResult,
+        );
+      });
+    });
+
+    describe('유저의 포인트가 충분할 때', () => {
+      //Give
+      const validId = 1;
+      const amount = 10000;
+      const userPoint = { id: validId, point: amount, updateMillis: 20 };
+      const usePoint = { id: validId, point: 0, updateMillis: 20 };
+
+      it('성공한다.', async () => {
+        pointRepoitory.getPointById.mockResolvedValue(userPoint);
+        pointRepoitory.upsertPoint.mockResolvedValue(usePoint);
+
+        expect(await pointService.use(validId, amount)).toEqual(usePoint);
+      });
+    });
   });
 });
