49f3ab6aafa546ab9a9154b085f08ff54026299d
vipwhy12 <vipwhy12@naver.com>
2024-09-27 05:33:18 +0900

    fix: 사용자 포인트 사용시, update 로직 수정

--- src/point/point.service.ts
+++ src/point/point.service.ts
@@ -1,14 +1,12 @@
-import { TransactionType } from '../point/point.model';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { PointHistory, UserPoint, TransactionType } from './point.model';
 import { PointRepository } from './point.repository';
-import { Injectable } from '@nestjs/common';
-import { PointHistory, UserPoint } from './point.model';
 
 @Injectable()
 export class PointService {
   constructor(private readonly pointRepository: PointRepository) {}
 
   async getPointById(id: number): Promise<UserPoint> {
-    //TODO - Custom Error Implements
     return await this.pointRepository.getPointById(id);
   }
 
@@ -19,9 +17,6 @@ export class PointService {
 
   //특정 유저의 포인트를 충전하는 기능
   async charge(id: number, amount: number): Promise<UserPoint> {
-    //TODO - Custom Error
-    //TODO - 동시성 제어: 동시성 제어란 공유자원을 두고 타투는 싸움!
-
     const transactionType = TransactionType.CHARGE;
     const userPoint = await this.pointRepository.upsertPoint(id, amount);
     await this.pointRepository.setHisotryId(
@@ -40,12 +35,14 @@ export class PointService {
     const userPoint = await this.pointRepository.getPointById(id);
     const point = userPoint.point;
 
-    if (point < amount) throw new Error(`${amount - point}point 부족`);
+    if (point < amount)
+      throw new BadRequestException(`${id}의 ${amount - point}point 부족`);
 
-    //TODO - Custom Error & 포인트가 충분하지 않은
-    //TODO - 동시성 제어: 동시성 제어란 공유자원을 두고 타투는 싸움!
+    const useUserPoint = await this.pointRepository.upsertPoint(
+      id,
+      amount - point,
+    );
 
-    const useUserPoint = await this.pointRepository.upsertPoint(id, amount);
     await this.pointRepository.setHisotryId(
       id,
       amount,
