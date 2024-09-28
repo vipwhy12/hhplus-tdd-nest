b55d5c6465c01e4779a858f4e166287e9def881f
vipwhy12 <vipwhy12@naver.com>
2024-09-29 02:01:52 +0900

    fix: 동시성 제어 서비스 레이어 변경

--- src/point/point.service.ts
+++ src/point/point.service.ts
@@ -1,10 +1,14 @@
 import { BadRequestException, Injectable } from '@nestjs/common';
 import { PointHistory, UserPoint, TransactionType } from './point.model';
 import { PointRepository } from './point.repository';
+import { Mutex } from '../mutex/mutex';
 
 @Injectable()
 export class PointService {
-  constructor(private readonly pointRepository: PointRepository) {}
+  constructor(
+    private readonly pointRepository: PointRepository,
+    private readonly mutex: Mutex,
+  ) {}
 
   async getPointById(id: number): Promise<UserPoint> {
     return await this.pointRepository.getPointById(id);
@@ -16,39 +20,43 @@ export class PointService {
   }
 
   //특정 유저의 포인트를 충전하는 기능
-  async charge(id: number, amount: number): Promise<UserPoint> {
-    const transactionType = TransactionType.CHARGE;
-    const userPoint = await this.pointRepository.upsertPoint(id, amount);
-    await this.pointRepository.setHisotryId(
-      id,
-      amount,
-      transactionType,
-      userPoint.updateMillis,
-    );
+  async charge(id: number, amount: number): Promise<any> {
+    return await this.mutex.isExistLock(id, async () => {
+      const transactionType = TransactionType.CHARGE;
+      const userPoint = await this.pointRepository.upsertPoint(id, amount);
+      await this.pointRepository.setHisotryId(
+        id,
+        amount,
+        transactionType,
+        userPoint.updateMillis,
+      );
 
-    return userPoint;
+      return userPoint;
+    });
   }
 
   //특정 유저의 포인트를 사용하는 기능
-  async use(id: number, amount: number): Promise<UserPoint> {
-    const transactionType: TransactionType = TransactionType.USE;
-    const userPoint = await this.pointRepository.getPointById(id);
-    const point = userPoint.point;
+  async use(id: number, amount: number): Promise<any> {
+    return await this.mutex.isExistLock(id, async () => {
+      const transactionType: TransactionType = TransactionType.USE;
+      const userPoint = await this.pointRepository.getPointById(id);
+      const point = userPoint.point;
 
-    if (point < amount)
-      throw new BadRequestException(`${id}의 ${amount - point}point 부족`);
+      if (point < amount)
+        throw new BadRequestException(`${id}의 ${amount - point}point 부족`);
 
-    const useUserPoint = await this.pointRepository.upsertPoint(
-      id,
-      point - amount,
-    );
+      const useUserPoint = await this.pointRepository.upsertPoint(
+        id,
+        point - amount,
+      );
 
-    await this.pointRepository.setHisotryId(
-      id,
-      amount,
-      transactionType,
-      useUserPoint.updateMillis,
-    );
-    return useUserPoint;
+      await this.pointRepository.setHisotryId(
+        id,
+        amount,
+        transactionType,
+        useUserPoint.updateMillis,
+      );
+      return useUserPoint;
+    });
   }
 }
